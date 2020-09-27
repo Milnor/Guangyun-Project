9ebe71ceaa2e0155883765a78004e6db990f7238
Seaver Milnor <b0rax@NaegleriaFowleri>
2020-09-26 23:57:21 -0400

    WIP - slow_search() started

--- ping_ze_poc.py
+++ ping_ze_poc.py
@@ -2,6 +2,49 @@
 
 import sys
 
+verbose = False
+debug = ""
+
+
+def add_candidates(zi, c_list, variants):
+
+	data = variants.read()
+	lines = data.split()
+
+	for line in lines:
+		characters = line.split(",")
+		first = characters[0]
+		second = characters[1]
+
+		if zi == first:
+			c_list.append(second)
+		elif zi == second:
+			c_list.append(first)
+
+# Repeat search with variants/allographs
+def slow_search(zi, shujuku):
+	
+	global verbose
+	global debug
+
+	sCandidates = []		# kSemanticVariants
+	zCandidates = []		# zVariants
+
+	if verbose:
+		debug += "[!] Attempting slow search on {}".format(zi) + "\n"
+
+	with open("data/s_variants.csv", 'r', encoding='utf-8') as sfile:
+		add_candidates(zi, sCandidates, sfile)
+
+	with open("data/z_variants.csv", 'r', encoding='utf-8') as zfile:
+		add_candidates(zi, zCandidates, zfile)
+
+	if verbose:
+		debug += "\tsVariants: {}\n".format(sCandidates)
+		debug += "\tzVariants: {}\n".format(zCandidates)
+
+	return "*"				# Not Found
+
 def ping_ze(zi, shujuku):
 
 	isPing = False
@@ -25,9 +68,12 @@ def ping_ze(zi, shujuku):
 	elif isPing and isZe:
 		return "?"			# Could be either
 	else:
-		return "* "			# Not Found
+		return slow_search(zi, shujuku) # Try Harder
 
 def main():
+
+	global verbose
+	global debug
 
 	# Open our Guang Yun database
 	gy = open("data/sbgy.txt", "r")
@@ -47,8 +93,11 @@ def main():
 	# volumes[0] = (blank)
 
 	# TODO: make this a command line argument
-	#poem = open("test_inputs/libai.txt", "r")
-	poem = open("test_inputs/dufu.txt", "r")	
+	poem = open("test_inputs/libai.txt", "r")
+	#poem = open("test_inputs/dufu.txt", "r")	
+
+	# TODO: make this a command line argument
+	verbose = True
 
 	i = 0
 	for line in poem:
@@ -57,6 +106,9 @@ def main():
 		for char in line.strip():
 			tones += ping_ze(char, volumes)
 		print("{} \t{} \t{}".format(str(i).zfill(4), line.strip(), tones))
+
+	if verbose:
+		print("\nDebug info\n----------\n{}".format(debug))
 
 	gy.close()
 	poem.close()
